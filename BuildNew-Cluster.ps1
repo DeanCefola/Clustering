d6bf34408d29890653e048ab6af01436173ff2af
Dean Cefola <Dean.Cefola@microsoft.com>
2020-09-04 21:54:59 -0400

    Create BuildNew-Cluster.ps1
    build new  cluster

---
+++ BuildNew-Cluster.ps1
@@ -0,0 +1,28 @@
+﻿<#Author   : Dean Cefola
+# Creation Date: 07-22-2020
+# Usage      : Setup FailOver Cluster
+
+#**************************************************************************
+# Date                         Version      Changes
+#------------------------------------------------------------------------
+# 07/22/2020                       1.0       Intial Version
+#
+#***************************************************************************
+#
+#>
+
+
+$nodes = ("Clus00","Clus01")
+Invoke-Command  $nodes {
+    Install-WindowsFeature Failover-Clustering `
+        -IncludeAllSubFeature `
+        -IncludeManagementTools
+}
+Test-Cluster `
+    –Node ("Clus00","Clus01") `
+    –Include "Inventory", "Network", "System Configuration"
+New-Cluster `
+    -Name AZCluster `
+    -Node ("Clus00","Clus01") `
+    -ManagementPointNetworkType Distributed
+
